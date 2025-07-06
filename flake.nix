1eaa78575e0e4a0db4031d27f590c68b693daec7
GlassGhost <roypfund@gmail.com>
2025-07-05 22:50:17 -0500

    Signed-off-by: GlassGhost <roypfund@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Simple OpenGL X11 demo using flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    packages.${system}.quad = pkgs.stdenv.mkDerivation {
+      pname = "quad";
+      version = "0.1";
+
+      src = ./.;
+      buildInputs = [
+        pkgs.xorg.libX11
+        pkgs.libGL
+        pkgs.libGLU
+      ];
+
+      buildPhase = ''
+        gcc -o quad quad.c -lX11 -lGL -lGLU
+      '';
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp quad $out/bin/
+      '';
+    };
+
+    # For local dev environment like your shell.nix
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = [
+        pkgs.gcc
+        pkgs.gnumake
+        pkgs.xorg.libX11.dev
+        pkgs.libGL
+        pkgs.libGLU
+      ];
+    };
+  };
+}
